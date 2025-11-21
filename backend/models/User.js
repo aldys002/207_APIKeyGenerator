d4c76a2ea19910f09ac9101091d81fd75a049e3a
aldys002 <igidiaaldys@gmail.com>
2025-11-21 13:47:59 +0700

    database user.js

---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require("sequelize");
+const db = require("../config/database");
+
+const User = db.define("User", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    firstname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    updated_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    }
+}, {
+    tableName: "users",
+    timestamps: false
+});
+
+module.exports = User;
